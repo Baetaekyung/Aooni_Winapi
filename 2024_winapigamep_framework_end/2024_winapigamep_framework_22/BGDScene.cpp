a128f177d9f50226a554523c2e4e70dbfee31e37
bigumdo <showmethe0610@gmail.com>
2024-12-01 01:18:18 +0900

    Enemy

--- 2024_winapigamep_framework_end/2024_winapigamep_framework_22/BGDScene.cpp
+++ 2024_winapigamep_framework_end/2024_winapigamep_framework_22/BGDScene.cpp
@@ -31,7 +31,7 @@ void BGDScene::Init()
 	//pObj3->SetName(L"BackGround2");
 	//AddObject(pObj3, LAYER::PROJECTILE);
 
-	GET_SINGLE(CollisionManager)->CheckLayer(LAYER::PLAYER, LAYER::ENEMY);
+	//GET_SINGLE(CollisionManager)->CheckLayer(LAYER::PLAYER, LAYER::ENEMY);
 	//GET_SINGLE(CollisionManager)->CheckLayer(LAYER::ENEMY, LAYER::PLAYER);
 	GET_SINGLE(CollisionManager)->CheckLayer(LAYER::ENEMY, LAYER::PROJECTILE);
 	//GET_SINGLE(CollisionManager)->CheckLayer(LAYER::PROJECTILE, LAYER::ENEMY);
